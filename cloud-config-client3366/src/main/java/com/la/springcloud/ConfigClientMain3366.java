409b5656195e6b5b61ac50bb24569db7c3834552
liao <1257695265@qq.com>
2022-12-09 18:24:20 +0800

    firth commit

---
+++ cloud-config-client3366/src/main/java/com/la/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,17 @@
+package com.la.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author LA
+ * @createDate 2022-12-09-10:59
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+}
